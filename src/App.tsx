0b2f5d905fbce20b2f6eea934044280c14827a95
Neyan <neyan7163529@gmail.com>
2023-05-16 22:59:37 +0500

    to review

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Nav from "./components/Nav";
+import MainSection from "./components/MainSection/MainSection";
+
+function App() {
+   return (
+      <>
+         <Nav />
+         <MainSection />
+      </>
+   );
+}
+
+export default App;
